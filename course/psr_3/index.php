5396931f772b44f1a3e7d9d2024e4cadbb3372ac
Kelver Machado <kelver_kmv@hotmail.com>
2017-03-02 23:46:49 -0400

    PSR 3 ## Implementando monolog para interface de logs de erro.

--- course/psr_3/index.php
+++ course/psr_3/index.php
@@ -5,3 +5,16 @@
  * Date: 02/03/17
  * Time: 23:40
  */
+
+require_once "vendor/autoload.php";
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('log/log.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
