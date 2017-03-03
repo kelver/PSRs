18559332eab4523fa39d2c58ab8a2c6fb95ae8d6
Kelver Machado <kelver_kmv@hotmail.com>
2017-03-03 00:22:04 -0400

    PSR 6 ##Implementando 'cache-util' via composer.

---
+++ course/psr_6/src/App/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kelver
+ * Date: 02/03/17
+ * Time: 23:57
+ */
+
+namespace App\Controller;
+
+
+class IndexController
+{
+    public function getController()
+    {
+        return "Controller: index";
+    }
+}
---
+++ course/psr_6/src/App/Hello.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kelver
+ * Date: 02/03/17
+ * Time: 23:53
+ */
+
+namespace App;
+
+
+class Hello
+{
+    public function getHello()
+    {
+        return "Hello!";
+    }
+}
