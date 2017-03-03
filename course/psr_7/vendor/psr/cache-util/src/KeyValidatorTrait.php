d72a84070d98f4b111b5a42c5bbb9c995969cbba
Kelver Machado <kelver_kmv@hotmail.com>
2017-03-03 00:27:52 -0400

    PSR 7 ##Instalando via composer 'http-message' para testes das interfaces http.

---
+++ course/psr_7/vendor/psr/cache-util/src/KeyValidatorTrait.php
@@ -0,0 +1,33 @@
+<?php
+namespace Fig\Cache;
+
+/**
+ * Key validator trait
+ */
+trait KeyValidatorTrait
+{
+    /**
+     * Determines if the specified key is legal under PSR-6.
+     *
+     * @param string $key
+     *   The key to validate.
+     * @throws InvalidArgumentException
+     *   An exception implementing The Cache InvalidArgumentException interface
+     *   will be thrown if the key does not validate.
+     * @return bool
+     *   TRUE if the specified key is legal.
+     */
+    protected function validateKey($key)
+    {
+        if (!is_string($key) || $key === '') {
+            throw new InvalidArgumentException('Key should be a non empty string');
+        }
+
+        $unsupportedMatched = preg_match('#[{}()/\\\@:]#', $key);
+        if ($unsupportedMatched > 0) {
+            throw new InvalidArgumentException('Can\'t validate the specified key');
+        }
+
+        return true;
+    }
+}
